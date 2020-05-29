32b8482c38c338cb4e2b6ae50becb0741c382f78
Jian Zhu <jian.zhu@iherb.com>
2020-05-29 08:48:05 +0800

    clear project

--- src/Ziana.IdsAdmin.Server/Startup.cs
+++ src/Ziana.IdsAdmin.Server/Startup.cs
@@ -1,18 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Blazorise;
 using Blazorise.AntDesign;
 using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Ziana.IdsAdmin.Server.Data;
+
 
 namespace Ziana.IdsAdmin.Server
 {
@@ -31,7 +25,6 @@ namespace Ziana.IdsAdmin.Server
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
 
             services
                 .AddBlazorise(options =>
